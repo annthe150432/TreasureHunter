b84361c7549046cea0d5f3bc725b7a879d4cd8ed
DatTTHE150411 <dattthe150411@fpt.edu.vn>
2022-03-26 21:29:34 +0700

    Change values for treasures and timer

--- Assets/Scripts/Clock/CountdownTimer.cs
+++ Assets/Scripts/Clock/CountdownTimer.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CountdownTimer : Timer
 {
-    public const float BaseDuration = 61;
+    public const float BaseDuration = 65;
 
     public float AddedDuration { get; set; } = 0;
 
